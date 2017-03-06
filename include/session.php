b47f1c6cf51f810557767accbfb10ad4f4fb699e
Milan Gajic <boromirsrb@yahoo.com>
2017-03-06 11:36:05 +0100

    finished session class

--- include/session.php
+++ include/session.php
@@ -3,25 +3,57 @@
 class Session {
     
     private $logged_in;
-    public $user_id;
-    public $message;
+    private $user_id;
+    private $message;
     
     function __construct(){
         session_start();
+        $this->check_login();
+        $this->check_message();
     }
     
     public function login($found_admin){
-        if($found_admin){
+        if ($found_admin){
             $this->logged_in = true;
             $this->user_id = $_SESSION['user_id'] = $found_admin->id;
         }
     }
     
     public function logout(){
-        if($this->user_id){
+        if ($this->user_id){
             unset($this->user_id);
             unset($_SESSION['user_id']);
             $this->logged_in = false;
+        }
+    }
+    
+    public function is_logged_in(){
+        return $this->logged_in;
+    }
+    
+    private function check_login(){
+        if (isset($_SESSION['user_id'])){
+            $this->logged_in = true;
+            $this->user_id = $_SESSION['user_id'];
+        } else {
+            unset($_SESSION['user_id']);
+            $this->logged_in = false;
+        }
+    }
+    
+    public function message($msg=""){
+        $_SESSION['message'] = $msg;
+        $this->message = $_SESSION['message'];
+        return $this->message;
+        
+    }
+
+    private function check_message(){
+        if (isset($_SESSION['message'])){
+            $this->message = $_SESSION['message'];
+            unset($_SESSION['message']);
+        } else {
+            $this->message = "";
         }
     }
     
